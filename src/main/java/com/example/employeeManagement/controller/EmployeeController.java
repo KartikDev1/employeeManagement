623374a502f8e2c3aab6896ec0dacfc918e588d0
KartikDev1 <raneeyy18@gmail.com>
2025-03-08 20:59:45 +0530

    Update EmployeeController.java

--- src/main/java/com/example/employeeManagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeeManagement/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://employeemanagementfrontend-production.up.railway.app")
 @RestController
 @RequestMapping("api/employees")
 public class EmployeeController {
